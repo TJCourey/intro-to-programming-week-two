85a39ace27bf10a1b9b92c6674ec3c1fbf552371
TJ Courey <tjcourey84@gmail.com>
2022-12-15 16:38:35 -0500

    end of thursday

--- frontend/gifting-frontend/src/app/features/counter/state/index.ts
+++ frontend/gifting-frontend/src/app/features/counter/state/index.ts
@@ -1,4 +1,5 @@
 import { ActionReducerMap, createFeatureSelector, createSelector } from "@ngrx/store";
+import { CountData } from "../models";
 
 
 import * as fromCount from './reducers/count-reducer'
@@ -16,4 +17,6 @@ const selectFeature = createFeatureSelector<CounterState>(featureName);
 const SelectCountBranch = createSelector(selectFeature, f => f.count);
 
 export const SelectCountCurrent = createSelector(SelectCountBranch, b=> b.current);
-export const SelectCountBy = createSelector(SelectCountBranch, b => b.by)
+export const SelectCountBy = createSelector(SelectCountBranch, b => b.by);
+
+export const selectCountData = createSelector(SelectCountBranch, b => b as CountData);
